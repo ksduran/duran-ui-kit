5ad7d9be9844c42229071db78bfa688c38e89a96
ksduran <155391050+ksduran@users.noreply.github.com>
2025-06-11 13:48:12 -0500

    Refactor DuranBackButton

--- duran_ui_kit/src/main/java/com/kevinduran/duran_ui_kit/buttons/DuranBackButton.kt
+++ duran_ui_kit/src/main/java/com/kevinduran/duran_ui_kit/buttons/DuranBackButton.kt
@@ -1,12 +1,34 @@
 package com.kevinduran.duran_ui_kit.buttons
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun DuranBackButton() {
-    Button(onClick = {}) {
-        Text("Back")
+fun DuranBackButton(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = { navController.popBackStack() },
+        modifier = modifier,
+        colors = IconButtonDefaults.iconButtonColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        elevation = IconButtonDefaults.iconButtonElevation(
+            defaultElevation = 15.dp
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.ArrowBackIosNew,
+            contentDescription = "back"
+        )
     }
 }
