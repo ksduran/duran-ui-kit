332129de76a6c6e16015ebdb36187acd8654f53e
ksduran <155391050+ksduran@users.noreply.github.com>
2025-06-11 14:13:55 -0500

    Fix DuranBackButton properties

--- duran_ui_kit/src/main/java/com/kevinduran/duran_ui_kit/buttons/DuranBackButton.kt
+++ duran_ui_kit/src/main/java/com/kevinduran/duran_ui_kit/buttons/DuranBackButton.kt
@@ -1,12 +1,32 @@
 package com.kevinduran.duran_ui_kit.buttons
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun DuranBackButton() {
-    Button(onClick = {}) {
-        Text("Back")
+fun DuranBackButton(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = { navController.popBackStack() },
+        modifier = modifier.shadow(15.dp),
+        colors = IconButtonDefaults.iconButtonColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+            contentDescription = "back"
+        )
     }
 }
